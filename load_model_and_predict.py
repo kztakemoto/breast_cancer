a3c2e523085772072c8dd5df5bb70e48f64526dd
kztakemoto <33507065+kztakemoto@users.noreply.github.com>
2021-06-16 08:55:09 +0900

    Update load_model_and_predict.py

--- load_model_and_predict.py
+++ load_model_and_predict.py
@@ -41,6 +41,6 @@ y_val = np.load('data/y_val.npy')
 preds = np.array(model.predict(X_val))
 
 # compute accuracy
-acc = np.sum(np.argmax(preds, axis = 1) == np.argmax(y_val, axis = 1)) / len(y_val)
+acc = np.sum(np.argmax(preds, axis=1) == np.argmax(y_val, axis=1)) / len(y_val)
 print(" Accuracy [val]: {:.2f}%".format(acc * 100.0))
 
